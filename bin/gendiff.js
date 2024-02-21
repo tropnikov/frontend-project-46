864bd3d0a06b32d45532794a4cea0a73661d37be
Maksim Tropnikov <makstropnikov@gmail.com>
2024-02-22 03:00:14 +0700

    feat: add option and arguments to help

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,6 +8,8 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
-  .version('1.0.0', '-V, --version', 'output the version number');
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse();

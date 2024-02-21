4605633faa514774145e4cd97629b852160af856
Maksim Tropnikov <makstropnikov@gmail.com>
2024-02-22 02:45:58 +0700

    init: initial commit

---
+++ bin/gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information')
+  .version('1.0.0', '-V, --version', 'output the version number');
+
+program.parse();
